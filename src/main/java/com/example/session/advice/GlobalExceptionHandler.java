4246fd4586d266f450d5c5a859674f7fd26020b5
dev-hjkim <dev--test@naver.com>
2022-10-01 21:02:25 +0900

    override 할 수 있는 다른 exception들 처리

--- src/main/java/com/example/session/advice/GlobalExceptionHandler.java
+++ src/main/java/com/example/session/advice/GlobalExceptionHandler.java
@@ -4,17 +4,28 @@ package com.example.session.advice;
 import com.example.session.common.dto.ExceptionResult;
 import com.example.session.common.dto.ResultType;
 import com.example.session.exception.BaseException;
+import org.springframework.beans.ConversionNotSupportedException;
+import org.springframework.beans.TypeMismatchException;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.http.converter.HttpMessageNotWritableException;
+import org.springframework.validation.BindException;
+import org.springframework.web.HttpMediaTypeNotAcceptableException;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingPathVariableException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.ServletRequestBindingException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.context.request.async.AsyncRequestTimeoutException;
+import org.springframework.web.multipart.support.MissingServletRequestPartException;
 import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -74,5 +85,91 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
                 ResultType.INVALID_PARAMETER.getStatus());
     }
 
+    @Override
+    protected ResponseEntity<Object> handleHttpMediaTypeNotSupported(HttpMediaTypeNotSupportedException ex,
+                                                                     HttpHeaders headers, HttpStatus status,
+                                                                     WebRequest request) {
+        return new ResponseEntity<>(new ExceptionResult(ResultType.UNSUPPORTED_MEDIA_TYPE),
+                ResultType.UNSUPPORTED_MEDIA_TYPE.getStatus());
+    }
 
+    @Override
+    protected ResponseEntity<Object> handleHttpMediaTypeNotAcceptable(HttpMediaTypeNotAcceptableException ex,
+                                                                      HttpHeaders headers, HttpStatus status,
+                                                                      WebRequest request) {
+        return new ResponseEntity<>(new ExceptionResult(ResultType.UNSUPPORTED_MEDIA_TYPE),
+                ResultType.UNSUPPORTED_MEDIA_TYPE.getStatus());
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMissingPathVariable(MissingPathVariableException ex,
+                                                               HttpHeaders headers, HttpStatus status,
+                                                               WebRequest request) {
+        return new ResponseEntity<>(new ExceptionResult(ResultType.MISSING_PARAMETER),
+                ResultType.MISSING_PARAMETER.getStatus());
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleServletRequestBindingException(ServletRequestBindingException ex,
+                                                                          HttpHeaders headers, HttpStatus status,
+                                                                          WebRequest request) {
+        return new ResponseEntity<>(new ExceptionResult(ResultType.INVALID_PARAMETER),
+                ResultType.INVALID_PARAMETER.getStatus());
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleConversionNotSupported(ConversionNotSupportedException ex,
+                                                                  HttpHeaders headers, HttpStatus status,
+                                                                  WebRequest request) {
+        return new ResponseEntity<>(new ExceptionResult(ResultType.NOT_ALLOWED_OPERATION),
+                ResultType.NOT_ALLOWED_OPERATION.getStatus());
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleTypeMismatch(TypeMismatchException ex,
+                                                        HttpHeaders headers, HttpStatus status,
+                                                        WebRequest request) {
+        return new ResponseEntity<>(new ExceptionResult(ResultType.INVALID_PARAMETER),
+                ResultType.INVALID_PARAMETER.getStatus());
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
+                                                                  HttpHeaders headers, HttpStatus status,
+                                                                  WebRequest request) {
+        return new ResponseEntity<>(new ExceptionResult(ResultType.INVALID_PARAMETER),
+                ResultType.INVALID_PARAMETER.getStatus());
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleHttpMessageNotWritable(HttpMessageNotWritableException ex,
+                                                                  HttpHeaders headers, HttpStatus status,
+                                                                  WebRequest request) {
+        return new ResponseEntity<>(new ExceptionResult(ResultType.NOT_ALLOWED_OPERATION),
+                ResultType.NOT_ALLOWED_OPERATION.getStatus());
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMissingServletRequestPart(MissingServletRequestPartException ex,
+                                                                     HttpHeaders headers, HttpStatus status,
+                                                                     WebRequest request) {
+        return new ResponseEntity<>(new ExceptionResult(ResultType.INVALID_PARAMETER),
+                ResultType.INVALID_PARAMETER.getStatus());
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleBindException(BindException ex,
+                                                         HttpHeaders headers, HttpStatus status,
+                                                         WebRequest request) {
+        return new ResponseEntity<>(new ExceptionResult(ResultType.INVALID_PARAMETER),
+                ResultType.INVALID_PARAMETER.getStatus());
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleAsyncRequestTimeoutException(AsyncRequestTimeoutException ex,
+                                                                        HttpHeaders headers, HttpStatus status,
+                                                                        WebRequest webRequest) {
+        return new ResponseEntity<>(new ExceptionResult(ResultType.SERVICE_UNAVAILABLE),
+                ResultType.SERVICE_UNAVAILABLE.getStatus());
+    }
 }
