357b86c61ecf8f6732a246f101f6ddac3d94f1ee
dev-hjkim <dev--test@naver.com>
2022-10-05 23:07:29 +0900

    미사용 변수 제거

--- src/test/java/com/example/session/post/controller/PostControllerTest.java
+++ src/test/java/com/example/session/post/controller/PostControllerTest.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -21,7 +20,6 @@ class PostControllerTest {
 
     private MockMvc mvc;
     MockHttpSession session;
-    MockHttpServletRequest request;
     private User user;
 
     @Autowired
